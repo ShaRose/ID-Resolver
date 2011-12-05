c86c26bd4ca8602849a9e3171879eac5964a3cb6
ShaRose <ShawnAndrewRose@Gmail.Com>
2011-12-04 22:07:03 -0330

    Added Testing mods.

---
+++ src/minecraft/net/minecraft/src/mod_TestMod1.java
@@ -0,0 +1,52 @@
+package net.minecraft.src;
+
+public class mod_TestMod1 extends BaseMod {
+
+	@Override
+	public void load() {
+		Item tempitem = null;
+		if(true)
+		{
+			tempitem = new Item(30000);
+			tempitem = new Item(30001).setItemName("TestItem1");
+			tempitem = new Item(30002).setItemName("TestItem2");
+			ModLoader.AddName(tempitem, "Test Item 2");
+		}
+		if(true)
+		{
+			tempitem = new ItemEgg(30003);
+			tempitem = new ItemEgg(30004).setItemName("TestItem4");
+			tempitem = new ItemEgg(30005).setItemName("TestItem5");
+			ModLoader.AddName(tempitem, "Test Item 5");
+		}
+		Block tempblock = null;
+		if(true)
+		{
+			tempblock = new Block(100, Material.cakeMaterial);
+			ModLoader.RegisterBlock(tempblock);
+			tempblock = new Block(101, Material.cakeMaterial)
+					.setBlockName("TestBlock1");
+			ModLoader.RegisterBlock(tempblock);
+			tempblock = new Block(102, Material.cakeMaterial)
+					.setBlockName("TestBlock2");
+			ModLoader.RegisterBlock(tempblock);
+			ModLoader.AddName(tempblock, "Test Block 2");
+		}
+		if(true)
+		{
+			tempblock = new BlockLog(103);
+			ModLoader.RegisterBlock(tempblock);
+			tempblock = new BlockLog(104).setBlockName("TestBlock4");
+			ModLoader.RegisterBlock(tempblock);
+			tempblock = new BlockLog(105).setBlockName("TestBlock5");
+			ModLoader.RegisterBlock(tempblock);
+			ModLoader.AddName(tempblock, "Test Block 5");
+		}
+	}
+	
+	@Override
+	public String getVersion() {
+		return "TEST ONLY";
+	}
+
+}
